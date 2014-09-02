6186bd927d3004ad008005ab19c102b071005df3
Winston Chang <winston@stdout.org>
2014-09-01 21:07:12 -0500

    Properly set up registration

--- R/registration.R
+++ R/registration.R
@@ -6,8 +6,6 @@ R_null <- function() .Call(C_null)
 #' @useDynLib rspeed C_identity
 R_identity <- function(x) .Call(C_identity, x)
 
-# FIXME: roxygen adds quotes around .registration=TRUE in NAMESPACE, but they
-#        shouldn't be there.
 #' @export
-#' @useDynLib rspeed .registration=TRUE
+#' @useDynLib rspeed, .registration=TRUE
 R_identity_reg <- function(x) .Call(C_identity_reg, x)
