2d8b88888536f401cb225cb6066d039d9faa74d3
Winston Chang <winston@stdout.org>
2014-09-01 21:08:56 -0500

    Registration doesn't actually work in this case

--- src/registration.c
+++ src/registration.c
@@ -18,12 +18,13 @@ SEXP C_identity_reg(SEXP x) {
   return x;
 }
 
-// Register the routines
-static const R_CallMethodDef callMethods[] = {
-  {"C_identity_reg", (DL_FUNC) &C_identity_reg, 1},
-  {NULL, NULL, 0}
-};
+// NOTE: this doesn't work - must be put in a separate package.
+// // Register the routines
+// static const R_CallMethodDef callMethods[] = {
+//   {"C_identity_reg", (DL_FUNC) &C_identity_reg, 1},
+//   {NULL, NULL, 0}
+// };
 
-void R_init_rspeed(DllInfo *info) {
-   R_registerRoutines(info, NULL, callMethods, NULL, NULL);
-}
+// void R_init_rspeed(DllInfo *info) {
+//    R_registerRoutines(info, NULL, callMethods, NULL, NULL);
+// }
