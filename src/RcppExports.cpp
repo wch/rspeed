5981be84a0ea4a885f8b93b661aff94ab846a5b8
Winston Chang <winston@stdout.org>
2016-02-18 15:21:05 -0600

    Update RcppExports

--- src/RcppExports.cpp
+++ src/RcppExports.cpp
@@ -6,49 +6,37 @@
 using namespace Rcpp;
 
 // asJSONNumeric
-CharacterVector asJSONNumeric(NumericVector x, int digits = 3, int precision = 0);
+CharacterVector asJSONNumeric(NumericVector x, int digits, int precision);
 RcppExport SEXP rspeed_asJSONNumeric(SEXP xSEXP, SEXP digitsSEXP, SEXP precisionSEXP) {
 BEGIN_RCPP
-    SEXP __sexp_result;
-    {
-        Rcpp::RNGScope __rngScope;
-        Rcpp::traits::input_parameter< NumericVector >::type x(xSEXP );
-        Rcpp::traits::input_parameter< int >::type digits(digitsSEXP );
-        Rcpp::traits::input_parameter< int >::type precision(precisionSEXP );
-        CharacterVector __result = asJSONNumeric(x, digits, precision);
-        PROTECT(__sexp_result = Rcpp::wrap(__result));
-    }
-    UNPROTECT(1);
-    return __sexp_result;
+    Rcpp::RObject __result;
+    Rcpp::RNGScope __rngScope;
+    Rcpp::traits::input_parameter< NumericVector >::type x(xSEXP);
+    Rcpp::traits::input_parameter< int >::type digits(digitsSEXP);
+    Rcpp::traits::input_parameter< int >::type precision(precisionSEXP);
+    __result = Rcpp::wrap(asJSONNumeric(x, digits, precision));
+    return __result;
 END_RCPP
 }
 // escape_one
 std::string escape_one(std::string x);
 RcppExport SEXP rspeed_escape_one(SEXP xSEXP) {
 BEGIN_RCPP
-    SEXP __sexp_result;
-    {
-        Rcpp::RNGScope __rngScope;
-        Rcpp::traits::input_parameter< std::string >::type x(xSEXP );
-        std::string __result = escape_one(x);
-        PROTECT(__sexp_result = Rcpp::wrap(__result));
-    }
-    UNPROTECT(1);
-    return __sexp_result;
+    Rcpp::RObject __result;
+    Rcpp::RNGScope __rngScope;
+    Rcpp::traits::input_parameter< std::string >::type x(xSEXP);
+    __result = Rcpp::wrap(escape_one(x));
+    return __result;
 END_RCPP
 }
 // escape_chars_cpp
 CharacterVector escape_chars_cpp(CharacterVector x);
 RcppExport SEXP rspeed_escape_chars_cpp(SEXP xSEXP) {
 BEGIN_RCPP
-    SEXP __sexp_result;
-    {
-        Rcpp::RNGScope __rngScope;
-        Rcpp::traits::input_parameter< CharacterVector >::type x(xSEXP );
-        CharacterVector __result = escape_chars_cpp(x);
-        PROTECT(__sexp_result = Rcpp::wrap(__result));
-    }
-    UNPROTECT(1);
-    return __sexp_result;
+    Rcpp::RObject __result;
+    Rcpp::RNGScope __rngScope;
+    Rcpp::traits::input_parameter< CharacterVector >::type x(xSEXP);
+    __result = Rcpp::wrap(escape_chars_cpp(x));
+    return __result;
 END_RCPP
 }
